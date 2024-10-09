28856a329612c1b05588b6758682c678634343e8
Marina Hoffmann <marinasthoffmann@gmail.com>
2024-10-09 20:07:27 +0200

    include data filter

--- src/App.tsx
+++ src/App.tsx
@@ -9,53 +9,11 @@ import { RecoilRoot } from 'recoil'
 
 function App() {
 
-  //   const [eventos, setEventos] = useState<IEvento[]>([
-  //     {
-  //         "descricao": "Estudar React",
-  //         "inicio": new Date("2024-07-23T09:00"),
-  //         "fim": new Date("2024-07-23T13:00"),
-  //         "completo": false,
-  //         "id": 1642342747
-  //     },
-  //     {
-  //         "descricao": "Estudar Recoil",
-  //         "inicio": new Date("2024-07-24T09:00"),
-  //         "fim": new Date("2024-07-24T11:00"),
-  //         "completo": false,
-  //         "id": 1642342959
-  //     }
-  // ])
-
-
   const [filtro, setFiltro] = useState<Date | null>()
 
   const adicionarEvento = (evento: IEvento) => {
     evento.id = Math.round((new Date()).getTime() / 1000)
-    // eventos.push(evento)
-    // console.log(eventos);
-
-    // setEventos([...eventos])
   }
-  // const alterarStatusEvento = (id: number) => {
-  //   // const evento = eventos.find(evento => evento.id === id)
-  //   // if (evento) {
-  //   //   evento.completo = !evento.completo
-  //   // }
-  //   // setEventos([...eventos])
-  // }
-  // const deletarEvento = (id: number) => {
-  //   // setEventos([...eventos.filter(evento => evento.id !== id)])
-  // }
-
-  const aplicarFiltro = (data: Date | null) => {
-    setFiltro(data)
-  }
-
-  // const filtrados = !filtro
-  //   ? eventos
-  //   : eventos.filter((evento) =>
-  //     filtro!.toISOString().slice(0, 10) === evento.inicio.toISOString().slice(0, 10)
-  //   );
 
   return (
     <RecoilRoot>
@@ -66,9 +24,7 @@ function App() {
           </Card>
           <hr />
           <Card>
-            <ListaDeEventos 
-              aoFiltroAplicado={aplicarFiltro} 
-            />
+            <ListaDeEventos />
           </Card>
         </div>
         <div className={style.Coluna}>
