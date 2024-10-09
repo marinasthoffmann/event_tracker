2efdd410fd859fde2eb27dcf3bcfdca3cbae5798
Marina Hoffmann <marinasthoffmann@gmail.com>
2024-10-09 19:00:40 +0200

    create excluirEvento hook

--- src/components/Evento/index.tsx
+++ src/components/Evento/index.tsx
@@ -2,16 +2,11 @@ import React from 'react';
 import { IEvento } from '../../interfaces/IEvento'
 import style from './Evento.module.scss';
 import EventoCheckbox from './EventoCheckbox';
-import { useSetRecoilState } from 'recoil';
-import { listaDeEventosState } from '../../state/atom';
+import useExcluirEvento from '../../state/hooks/useExcluirEvento';
 
 const Evento: React.FC<{ evento: IEvento }> = ({ evento }) => {
   
-  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState)
-  
-  const excluirEvento = () => {
-    setListaDeEventos(listaAntiga => listaAntiga.filter(evt => evt.id !== evento.id))
-  }
+  const excluirEvento = useExcluirEvento()
 
   const estilos = [
     style.Evento
@@ -27,7 +22,7 @@ const Evento: React.FC<{ evento: IEvento }> = ({ evento }) => {
     <div className="cards-info">
       <h3 className={style.descricao}>{evento.descricao} - {evento.inicio.toLocaleDateString()}</h3>
     </div>
-    <i className="far fa-times-circle fa-2x" onClick={excluirEvento}></i>
+    <i className="far fa-times-circle fa-2x" onClick={() => excluirEvento(evento)}></i>
   </div>)
 }
 
